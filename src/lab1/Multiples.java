ae52d9d73436b75bb7a541ac2297789fdf698c08
dma40 <dma99878@gmail.com>
2025-09-09 12:46:58 -0400

    run test files for finished Multiples file

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,5 +1,27 @@
 package lab1;
 
 public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(multiples());
+    }
 
+    public static int multiples(int n, int a, int b) {
+        int lcm = (a * b) / gcd(a, b);
+
+        return (n - 1) / a + (n - 1) / b - (n - 1) / lcm;
+    }
+
+    public static int multiples() {
+        return 999 / 3 + 999 / 5 - 999 / 15;
+    }
+
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
+
+        return a;
+    }
 }
